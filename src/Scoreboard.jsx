2fef39dd008179cd05aea816f0e369474263791d
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-11-14 15:51:37 -0500

    Use semantic section for scoreboard

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,10 +1,10 @@
 ﻿export default function Scoreboard({wins, losses, ties, reset}) {
     return (
-        <div className={"scoreboard"}>
+        <section className={"scoreboard"}>
             <p>Wins: {wins}</p>
             <p>Ties: {ties}</p>
             <p>Losses: {losses}</p>
             <button onClick = {reset}>Reset</button>
-        </div>
+        </section>
     )
 }
